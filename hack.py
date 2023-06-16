855e4b25613012aa7e7c103103e2bece90f4a9a5
Will Ware <wware@veracode.com>
2023-06-16 10:37:46 -0400

    plod

--- hack.py
+++ hack.py
@@ -1,24 +1,31 @@
 # Flask server to test this out
 from flask import Flask, render_template, request, redirect, url_for
 from markupsafe import escape
+import urllib
+import json
 
 app = Flask(__name__)
+def qp(u):
+    if isinstance(u, str):
+        return urllib.parse.quote_plus(u)
+    return ""
+app.jinja_env.filters['quote_plus'] = qp
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def hello():
-    return render_template('index.html')
-
-
-@app.route('/note', methods=['GET', 'POST'])
-def note():
-    foo = request.form['foo']
-    if not foo:
-        # if foo is None or empty string, try to read the MD file
-        try:
-            foo = open("foo.md", "r").read()
-        except:
-            pass
-    else:
+    try:
+        foo = request.form['foo']
+    except:
+        foo = None
+    if isinstance(foo, str):
         # save foo to the MD file to pick up later
         open("foo.md", "w").write(foo)
+    else:
+        # if foo is None or empty string, try to read the MD file
+        try:
+            R = open("foo.md", "r").read()
+            foo = urllib.parse.quote(R)
+        except:
+            foo = None
     return render_template('index.html', value=foo, note="1")
